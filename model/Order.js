468be4e1ccb600c42192158b5d1ae72a8f1e91d8
Salman AHmed <shahsalmanahmed009@gmail.com>
2022-08-04 23:49:15 +0600

    added schema on model folder

---
+++ model/Order.js
@@ -0,0 +1,19 @@
+const { default: mongoose } = require("mongoose");
+
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: { type: String },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+    address: { type: String, required: true },
+    amount: { type: Number, required: true },
+    address: { type: String, Default: "Pending", required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", OrderSchema);
